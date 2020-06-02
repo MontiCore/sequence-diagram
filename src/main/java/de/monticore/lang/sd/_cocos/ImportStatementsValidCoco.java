058a1dfd042627773803609818afa18437fff7e2
Christian Broering <christian.broering@rwth-aachen.de>
2020-06-02 13:14:32 +0200

    importStatement coco fixed

--- src/main/java/de/monticore/lang/sd/_cocos/ImportStatementsValidCoco.java
+++ src/main/java/de/monticore/lang/sd/_cocos/ImportStatementsValidCoco.java
@@ -2,45 +2,64 @@
 
 package de.monticore.lang.sd._cocos;
 
+import de.monticore.expressions.oclexpressions._symboltable.IOCLExpressionsGlobalScope;
 import de.monticore.lang.sd._ast.ASTSDArtifact;
 import de.monticore.lang.sd._ast.ASTSDImportStatement;
-import de.monticore.lang.sd._ast.ASTSDPackageDeclaration;
+import de.monticore.types.mcbasictypes._ast.ASTMCImportStatement;
+import de.monticore.types.mcbasictypes._ast.ASTMCQualifiedName;
 import de.se_rwth.commons.logging.Log;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class ImportStatementsValidCoco implements SDASTSDArtifactCoCo {
 
   @Override
-  public void check(ASTSDArtifact node) {
+  public void check(ASTSDArtifact artifact) {
 
-    String path = node.getPath();
-    String fileName = node.getFileName();
+    String path = artifact.getPath();
+    String fileName = artifact.getFileName();
     String root = path;
 
     // Package declaration present?
-    if (node.isPresentPackageDeclaration()) {
-      ASTSDPackageDeclaration packageDeclaration = node.getPackageDeclaration();
-      String packageName = packageDeclaration.getQualifiedName().toString();
+    if (artifact.isPresentMCQualifiedName()) {
+      ASTMCQualifiedName packageDeclaration = artifact.getMCQualifiedName();
+      String packageName = packageDeclaration.toString();
       PackageNameIsFolderNameCoco coco2 = new PackageNameIsFolderNameCoco();
       // Check if package name is valid
-      if (!coco2.isSuffix(path, packageName)) {
+      if (!isSuffix(path, packageName)) {
         Log.error(this.getClass().getSimpleName() + ": Cannot resolve import statements. The package name "
             + packageName + " of " + path + "/" + fileName + " does not match its path on the file system",
-            node.get_SourcePositionStart());
+            artifact.get_SourcePositionStart());
       }
       root = path.replaceAll(packageName.replaceAll("\\.", "/"), "");
     }
 
+
     // Check if for each import statement there exists a file
-    for (ASTSDImportStatement imp : node.getImportStatementsList()) {
-      File file = new File(root + imp.getPath(), imp.getFileName());
-      if (!file.exists()) {
+    for (ASTMCImportStatement imp : artifact.getMCImportStatementList()) {
+      try {
+        Stream<Path> pathStream = Files.list(Paths.get(root + imp.getQName()));
+        pathStream.anyMatch(x -> x.toString() == imp.toString());
+      } catch (IOException exception) {
+        Log.error("")
+      }
+
+      Files.exists(Paths.get(root + imp.getQName()));
+      File file = new File(root + imp.getQName(), imp.getFileName());
+      if (!file.exists(Paths.get())) {
         Log.error(this.getClass().getSimpleName() + ": Cannot find file " + file.getAbsolutePath()
             + " referenced in import statement", imp.get_SourcePositionStart());
       }
     }
-
+  }
+  private boolean isSuffix(String path, String packageName) {
+    String pathWithDots = path.replaceAll("/", ".");
+    return pathWithDots.endsWith("." + packageName);
   }
 
 }
