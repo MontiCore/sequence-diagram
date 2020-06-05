c165b003415ff5a1f8cf4398133cd54d58e3c57b
Christian Broering <christian.broering@rwth-aachen.de>
2020-06-05 21:19:49 +0200

    coco for object name and object type naming convention added

---
+++ src/main/java/de/monticore/lang/sd/_coco/core/ObjectNameNamingConventionCoco.java
@@ -0,0 +1,28 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+package de.monticore.lang.sd._coco.core;
+
+import de.monticore.lang.sdcore._ast.ASTObject;
+import de.monticore.lang.sdcore._cocos.SDCoreASTObjectCoCo;
+import de.se_rwth.commons.logging.Log;
+
+public class ObjectNameNamingConventionCoco implements SDCoreASTObjectCoCo {
+
+  @Override
+  public void check(ASTObject object) {
+    if(object.isPresentName()) {
+      String name = object.getName();
+      if(name.isEmpty()) {
+        Log.warn("Objectdeclaration introduces an object with name"
+                + name
+                + " which should either not be empty or not set");
+      }
+      if (Character.isUpperCase(name.charAt(0))) {
+        Log.warn("Objectdeclaration introduces an object with name "
+                + name
+                + " which should be written lower case by convention"
+        );
+      }
+    }
+  }
+}
---
+++ src/main/java/de/monticore/lang/sd/_coco/core/ObjectTypeNamingConventionCoco.java
@@ -0,0 +1,32 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+package de.monticore.lang.sd._coco.core;
+
+import de.monticore.lang.sdcore._ast.ASTObject;
+import de.monticore.lang.sdcore._cocos.SDCoreASTObjectCoCo;
+import de.monticore.prettyprint.IndentPrinter;
+import de.monticore.types.mcbasictypes._ast.ASTMCObjectType;
+import de.monticore.types.prettyprint.MCBasicTypesPrettyPrinter;
+import de.se_rwth.commons.logging.Log;
+
+public class ObjectTypeNamingConventionCoco implements SDCoreASTObjectCoCo {
+
+  private final MCBasicTypesPrettyPrinter prettyPrinter;
+  public ObjectTypeNamingConventionCoco() {
+    this.prettyPrinter = new MCBasicTypesPrettyPrinter(new IndentPrinter());
+  }
+
+  @Override
+  public void check(ASTObject object) {
+    if(object.isPresentMCObjectType()) {
+      ASTMCObjectType mcObjectType = object.getMCObjectType();
+      String objectTypeName = mcObjectType.printType(prettyPrinter);
+      if (Character.isLowerCase(objectTypeName.charAt(0))) {
+        Log.warn("Objectdeclaration introduces an object with name "
+                + objectTypeName
+                + " which should be written upper case by convention.", object.get_SourcePositionStart()
+        );
+      }
+    }
+  }
+}
