fcf081600f47b45eb91f825c7174b15e56cbff4e
Christian Broering <christian.broering@rwth-aachen.de>
2020-06-06 11:44:50 +0200

    tests added

---
+++ src/test/java/de/monticore/lang/sd/cocos/ReferencedObjectsDeclaredCocoTest.java
@@ -0,0 +1,37 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+package de.monticore.lang.sd.cocos;
+
+import static org.junit.Assert.assertEquals;
+
+import de.monticore.lang.sd._ast.ASTSDArtifact;
+import de.monticore.lang.sd._cocos.ReferencedObjectsDeclaredCoco;
+import de.monticore.lang.sd._cocos.SDCoCoChecker;
+import de.se_rwth.commons.logging.Log;
+
+public class ReferencedObjectsDeclaredCocoTest extends SDCocoTest {
+
+  @Override
+  protected void initCoCoChecker() {
+    checker = new SDCoCoChecker();
+    checker.addCoCo(new ReferencedObjectsDeclaredCoco());
+  }
+
+  @Override
+  public void testCocoViolation() {
+    ASTSDArtifact sd = loadModel(INCORRECT_PATH, "reference_undeclared_objects.sd");
+    checker.checkAll(sd);
+    assertEquals(6, Log.getErrorCount());
+    assertEquals(6,
+        Log.getFindings().stream().filter(f -> f.buildMsg().contains("ReferencedObjectsDeclaredCoco")).count());
+  }
+
+  @Override
+  public void testCorrectExamples() {
+    testAllCorrectExamples();
+    assertEquals(0, Log.getErrorCount());
+    assertEquals(0,
+        Log.getFindings().stream().filter(f -> f.buildMsg().contains("ReferencedObjectsDeclaredCoco")).count());
+  }
+
+}
---
+++ src/test/java/de/monticore/lang/sd/cocos/SDCocoTest.java
@@ -0,0 +1,76 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+package de.monticore.lang.sd.cocos;
+
+import de.monticore.lang.sd._ast.ASTSDArtifact;
+import de.monticore.lang.sd._cocos.SDCoCoChecker;
+import de.monticore.lang.sd._symboltable.SDLanguage;
+import de.se_rwth.commons.logging.Log;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SDCocoTest {
+
+  private final static String CORRECT_PATH = "src/test/resources/examples/correct/";
+  protected final static String INCORRECT_PATH = "src/test/resources/examples/incorrect/";
+
+  protected SDCoCoChecker checker;
+
+  public SDCocoTest() {
+    Log.enableFailQuick(false);
+    initCoCoChecker();
+  }
+
+  @After
+  public void clearLog() {
+    Log.getFindings().clear();
+  }
+
+  protected abstract void initCoCoChecker();
+
+  @Test
+  public abstract void testCocoViolation();
+
+  @Test
+  public abstract void testCorrectExamples();
+
+  protected void testAllCorrectExamples() {
+    for (ASTSDArtifact sd : getAllCorrectExamples()) {
+      initCoCoChecker(); // Reset after each model check
+      checker.checkAll(sd);
+    }
+  }
+
+  private List<ASTSDArtifact> getAllCorrectExamples() {
+    List<ASTSDArtifact> examples = new ArrayList<ASTSDArtifact>();
+    // Lecture examples
+    examples.add(loadModel(CORRECT_PATH + "lecture", "example_1.sd"));
+    examples.add(loadModel(CORRECT_PATH + "lecture", "example_2_interactions.sd"));
+    examples.add(loadModel(CORRECT_PATH + "lecture", "example_3_static.sd"));
+    examples.add(loadModel(CORRECT_PATH + "lecture", "example_4_constructor.sd"));
+    examples.add(loadModel(CORRECT_PATH + "lecture", "example_5_factory.sd"));
+    examples.add(loadModel(CORRECT_PATH + "lecture", "example_6_stereotypes.sd"));
+    examples.add(loadModel(CORRECT_PATH + "lecture", "example_7_noocl.sd"));
+    //    examples.add(loadModel(CORRECT_PATH + "lecture", "example_7_ocl.sd")); //TODO: Use the real one once OCL is back
+
+    // examples.add(loadModel(CORRECT_PATH + "lecture",
+    // "example_8_ocl_let.sd"));
+    examples.add(loadModel(CORRECT_PATH + "lecture", "example_9_non_causal.sd"));
+    // Own examples
+    examples.add(loadModel(CORRECT_PATH, "example.sd"));
+    examples.add(loadModel(CORRECT_PATH, "example_completeness_and_stereotypes.sd"));
+    examples.add(loadModel(CORRECT_PATH, "allGrammarElements.sd"));
+    examples.add(loadModel(CORRECT_PATH, "activities.sd"));
+    return examples;
+  }
+
+  protected ASTSDArtifact loadModel(String path, String model) {
+    SDLanguage lang = new SDLanguage();
+    System.err.println("Loading model: " + path + "/" + model);
+    return lang.loadModelWithoutCocos(path, model);
+  }
+
+}
