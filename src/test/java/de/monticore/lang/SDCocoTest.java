41e0119471246758a153f27d0be1e847b4abac8f
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-06-07 14:47:22 +0200

    changed to ParameterizedTests

--- src/test/java/de/monticore/lang/SDCocoTest.java
+++ src/test/java/de/monticore/lang/SDCocoTest.java
@@ -9,10 +9,13 @@ import de.se_rwth.commons.logging.Log;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -47,56 +50,42 @@ public abstract class SDCocoTest {
   protected abstract Class<?> getCoCoUnderTest();
 
   protected void testCocoViolation(String modelName, int errorCount, int logFindingsCount) {
-    ASTSDArtifact sd = loadModel(INCORRECT_PATH, modelName);
+    ASTSDArtifact sd = loadModel(INCORRECT_PATH + "/" + modelName);
     checker.checkAll(sd);
     assertEquals(errorCount, Log.getErrorCount());
     assertEquals(logFindingsCount,
             Log.getFindings().stream().filter(f -> f.buildMsg().contains(getCoCoUnderTest().getSimpleName())).count());
   }
 
-  @Test
-  public void testCorrectExamples() {
-    testAllCorrectExamples();
+  @ParameterizedTest
+  @CsvSource({
+          "lecture/example_1.sd",
+          "lecture/example_2_interactions.sd",
+          "lecture/example_3_static.sd",
+          "lecture/example_4_constructor.sd",
+          "lecture/example_5_factory.sd",
+          "lecture/example_6_stereotypes.sd",
+          "lecture/example_7_noocl.sd",
+          // "lecture/example_8_ocl_let.sd",
+          "lecture/example_9_non_causal.sd",
+          // "example.sd,
+          "example_completeness_and_stereotypes.sd",
+          "allGrammarElements.sd",
+          "activities.sd"
+  })
+  public void testCorrectExamples(String model) {
+    ASTSDArtifact sd = loadModel(CORRECT_PATH + model);
+    checker.checkAll(sd);
     assertEquals(0, Log.getErrorCount());
     assertEquals(0,
             Log.getFindings().stream().filter(f -> f.buildMsg().contains(getCoCoUnderTest().getSimpleName())).count());
   }
 
-  protected void testAllCorrectExamples() {
-    for (ASTSDArtifact sd : getAllCorrectExamples()) {
-      initCoCoChecker(); // Reset after each model check
-      checker.checkAll(sd);
-    }
-  }
-
-  private List<ASTSDArtifact> getAllCorrectExamples() {
-    List<ASTSDArtifact> examples = new ArrayList<ASTSDArtifact>();
-    // Lecture examples
-    examples.add(loadModel(CORRECT_PATH + "lecture", "example_1.sd"));
-    examples.add(loadModel(CORRECT_PATH + "lecture", "example_2_interactions.sd"));
-    examples.add(loadModel(CORRECT_PATH + "lecture", "example_3_static.sd"));
-    examples.add(loadModel(CORRECT_PATH + "lecture", "example_4_constructor.sd"));
-    examples.add(loadModel(CORRECT_PATH + "lecture", "example_5_factory.sd"));
-    examples.add(loadModel(CORRECT_PATH + "lecture", "example_6_stereotypes.sd"));
-    examples.add(loadModel(CORRECT_PATH + "lecture", "example_7_noocl.sd"));
-    //    examples.add(loadModel(CORRECT_PATH + "lecture", "example_7_ocl.sd")); //TODO: Use the real one once OCL is back
-
-    // examples.add(loadModel(CORRECT_PATH + "lecture",
-    // "example_8_ocl_let.sd"));
-    examples.add(loadModel(CORRECT_PATH + "lecture", "example_9_non_causal.sd"));
-    // Own examples
-    // examples.add(loadModel(CORRECT_PATH, "example.sd"));
-    examples.add(loadModel(CORRECT_PATH, "example_completeness_and_stereotypes.sd"));
-    examples.add(loadModel(CORRECT_PATH, "allGrammarElements.sd"));
-    examples.add(loadModel(CORRECT_PATH, "activities.sd"));
-    return examples;
-  }
-
-  protected ASTSDArtifact loadModel(String path, String model) {
+  protected ASTSDArtifact loadModel(String modelPath) {
     try {
-      return parser.parse(path + "/" + model).get();
-    } catch (IOException | NullPointerException e) {
-      System.err.println("Loading model: " + path + "/" + model + " failed: " + e.getMessage());
+      return parser.parse(modelPath).orElseThrow(NoSuchElementException::new);
+    } catch (IOException | NoSuchElementException e) {
+      System.err.println("Loading model: " + modelPath + " failed: " + e.getMessage());
       fail();
     }
     return null;
