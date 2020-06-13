5cb5b32c2b06c866279bca39d21f35da88ff9b0d
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-06-13 12:17:21 +0200

    add parser test

---
+++ src/test/java/de/monticore/lang/SDParserTest.java
@@ -0,0 +1,79 @@
+package de.monticore.lang;
+
+import de.monticore.lang.sd4development._parser.SD4DevelopmentParser;
+import de.monticore.lang.sdbasis._ast.ASTSDArtifact;
+import de.se_rwth.commons.logging.Log;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SDParserTest {
+
+  private final static String CORRECT_PATH = "src/test/resources/examples/correct/";
+  protected final static String INCORRECT_PATH = "src/test/resources/examples/incorrect/";
+
+  private SD4DevelopmentParser parser;
+
+  @BeforeEach
+  void setup() {
+    this.parser = new SD4DevelopmentParser();
+    Log.enableFailQuick(false);
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+          "lecture/example_1.sd",
+          "lecture/example_2_interactions.sd",
+          "lecture/example_3_static.sd",
+          "lecture/example_4_constructor.sd",
+          "lecture/example_5_factory.sd",
+          "lecture/example_6_stereotypes.sd",
+          "lecture/example_7_noocl.sd",
+          // "lecture/example_8_ocl_let.sd",
+          "lecture/example_9_non_causal.sd",
+          // "example.sd,
+          "example_completeness_and_stereotypes.sd",
+          "allGrammarElements.sd",
+          "activities.sd"
+  })
+  void testCorrectExamples(String model) {
+    testParseModel(CORRECT_PATH, model);
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+          "artifact_not_sd_name.sd",
+          "completeness_inconsistent.sd",
+          "faulty_imports.sd",
+          "incomplete_not_allowed.sd",
+          "inline_objectdefinition_without_constructor.sd",
+          "no_unique_names.sd",
+          "ocl_context_not_declared.sd",
+          "reference_undeclared_objects.sd",
+          "return_before_method.sd",
+          "static_method_refers_to_object.sd",
+          "uncommon_file_extension.sy",
+          "violated_naming_conventions.sd",
+          "wrong_package.sd"
+  })
+  void testInCorrectExamples(String model) {
+    testParseModel(INCORRECT_PATH, model);
+  }
+
+  private void testParseModel(String path, String model) {
+    try {
+      Optional<ASTSDArtifact> ast = parser.parse(path + model);
+      assertTrue(ast.isPresent());
+    } catch (IOException | NoSuchElementException e) {
+      System.err.println("Loading model: " + model + " failed: " + e.getMessage());
+      fail();
+    }
+  }
+}
