9fd785839ce1aeeef474a0864cd78c09aad11c07
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-06-21 16:27:00 +0200

    TypeNamingConventionCoco.java

--- src/main/java/de/monticore/lang/sdbasis/_cocos/TypeNamingConventionCoco.java
+++ src/main/java/de/monticore/lang/sdbasis/_cocos/TypeNamingConventionCoco.java
@@ -4,11 +4,13 @@ package de.monticore.lang.sdbasis._cocos;
 
 import de.monticore.prettyprint.IndentPrinter;
 import de.monticore.types.mcbasictypes._ast.ASTMCObjectType;
+import de.monticore.types.mcbasictypes._ast.ASTMCQualifiedType;
 import de.monticore.types.mcbasictypes._cocos.MCBasicTypesASTMCObjectTypeCoCo;
+import de.monticore.types.mcbasictypes._cocos.MCBasicTypesASTMCQualifiedTypeCoCo;
 import de.monticore.types.prettyprint.MCBasicTypesPrettyPrinter;
 import de.se_rwth.commons.logging.Log;
 
-public class TypeNamingConventionCoco implements MCBasicTypesASTMCObjectTypeCoCo {
+public class TypeNamingConventionCoco implements MCBasicTypesASTMCQualifiedTypeCoCo {
 
   static final String MESSAGE_WARNING_UPPER_CASE = TypeNamingConventionCoco.class.getSimpleName() + ": "
           + "Object declaration introduces an object with name %s "
@@ -21,8 +23,8 @@ public class TypeNamingConventionCoco implements MCBasicTypesASTMCObjectTypeCoCo
   }
 
   @Override
-  public void check(ASTMCObjectType mcObjectType) {
-      String objectTypeName = mcObjectType.printType(prettyPrinter);
+  public void check(ASTMCQualifiedType mcObjectType) {
+      String objectTypeName = mcObjectType.getNameList().get(mcObjectType.getNameList().size() - 1);
       if (Character.isLowerCase(objectTypeName.charAt(0))) {
         Log.warn(String.format(MESSAGE_WARNING_UPPER_CASE, objectTypeName), mcObjectType.get_SourcePositionStart()
         );
