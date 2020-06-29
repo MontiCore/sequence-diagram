fa9fc5e70c66e2f7c0bec0eb57f648f3af99372b
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-06-29 18:46:41 +0200

    added test for deser

--- src/test/java/de/monticore/lang/sd4development/_symboltable/deser/SDDeSerTest.java
+++ src/test/java/de/monticore/lang/sd4development/_symboltable/deser/SDDeSerTest.java
@@ -4,21 +4,28 @@ import de.monticore.io.paths.ModelPath;
 import de.monticore.lang.sd4development._parser.SD4DevelopmentParser;
 import de.monticore.lang.sd4development._symboltable.*;
 import de.monticore.lang.sdbasis._ast.ASTSDArtifact;
+import de.monticore.lang.sdbasis._symboltable.SequenceDiagramSymbol;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class SDDeSerTest {
 
   private static final String MODEL_PATH = "src/test/resources/";
 
-  private static final String CORRECT_PATH = MODEL_PATH + "examples/correct/";
+  private static final String PACKAGE_PATH = MODEL_PATH + "examples/symboltable/deser/";
 
   private final SD4DevelopmentParser parser = new SD4DevelopmentParser();
 
@@ -33,21 +40,43 @@ public class SDDeSerTest {
             .setModelFileExtension(SD4DevelopmentGlobalScope.FILE_EXTENSION)
             .build();
     this.deSer = new SD4DevelopmentScopeDeSer();
+    this.deSer.setSymbolFileExtension("sdsym");
   }
 
   @ParameterizedTest
   @CsvSource(
-          "lecture/example_1.sd"
+          "example_1.sd"
   )
   void testSerialization(String model) {
     // given
-    ASTSDArtifact ast = loadModel(CORRECT_PATH + model);
+    ASTSDArtifact ast = loadModel(PACKAGE_PATH + model);
+    assertNotNull(ast);
 
     // when
     String serializedSD = deSer.serialize((SD4DevelopmentArtifactScope) ast.getEnclosingScope());
 
     // then
     System.out.println(serializedSD);
+    assertTrue(serializedSD.length() > 0);
+  }
+
+  @Disabled("This test currently fails, because the DeSer of SD4Development does not recognize that SDArtifact has seqeuenceDiagramSymbols")
+  @Test
+  void testDeserialization() {
+    // given
+    String serializedSD = loadSerializedModel(Paths.get(PACKAGE_PATH, "example_1.sdsym"));
+    assertNotNull(serializedSD);
+
+    // when
+    ISD4DevelopmentScope deserializedSD = deSer.deserialize(serializedSD);
+
+    // then
+    assertNotNull(deserializedSD);
+    assertEquals(1, deserializedSD.getLocalSequenceDiagramSymbols().size());
+    SequenceDiagramSymbol sdSymbol = deserializedSD.getLocalSequenceDiagramSymbols().get(0);
+    assertEquals("example_1", sdSymbol.getName());
+    assertEquals("examples.symboltable.deser", sdSymbol.getPackageName());
+    assertEquals(1, deserializedSD.getSubScopes().size());
   }
 
   private ASTSDArtifact loadModel(String modelPath) {
@@ -57,6 +86,16 @@ public class SDDeSerTest {
       return ast;
     } catch (IOException | NoSuchElementException e) {
       System.err.println("Loading model: " + modelPath + " failed: " + e.getMessage());
+      fail();
+    }
+    throw new IllegalStateException("Something went wrong..");
+  }
+
+  private String loadSerializedModel(Path serializedPath) {
+    try (Stream<String> lines = Files.lines(serializedPath)) {
+      return lines.collect(Collectors.joining(System.lineSeparator()));
+    } catch (IOException e) {
+      System.err.println("Loading serialized model: " + serializedPath + " failed: " + e.getMessage());
       fail();
     }
     throw new IllegalStateException("Something went wrong..");
