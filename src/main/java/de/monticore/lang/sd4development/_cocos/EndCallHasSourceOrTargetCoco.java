e8976decb448af7ecf801428872f3515ae25aeb2
Christian Broering <christian.broering@rwth-aachen.de>
2020-06-29 11:35:38 +0200

    checks if either SDSource or SDTarget is set

---
+++ src/main/java/de/monticore/lang/sd4development/_cocos/EndCallHasSourceOrTargetCoco.java
@@ -0,0 +1,21 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+package de.monticore.lang.sd4development._cocos;
+
+import de.monticore.lang.sd4development._ast.ASTSDEndCall;
+import de.monticore.lang.sdbasis._ast.ASTSDSendMessage;
+import de.monticore.lang.sdbasis._cocos.SDBasisASTSDSendMessageCoCo;
+import de.se_rwth.commons.logging.Log;
+
+public class EndCallHasSourceOrTargetCoco implements SD4DevelopmentASTSDEndCallCoCo {
+
+  static final String MESSAGE_ERROR = EndCallHasSourceOrTargetCoco.class.getSimpleName() + ": "
+          + "The interaction has neither a source nor a target. At least one of them must be set.";
+  
+  @Override
+  public void check(ASTSDEndCall object) {
+    if(!object.isPresentSDTarget() && !object.isPresentSDSource()) {
+      Log.error(MESSAGE_ERROR);
+    }
+  }
+}
