aed9156ec759521f2a8cc690a1d2621eb5c9c2b7
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2020-07-14 15:13:36 +0200

    create coco + test

---
+++ src/test/java/de/monticore/lang/sdbasis/_cocos/ReferencedTypeExistsCocoTest.java
@@ -0,0 +1,25 @@
+package de.monticore.lang.sdbasis._cocos;
+
+import de.monticore.lang.SDCocoTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReferencedTypeExistsCocoTest extends SDCocoTest {
+
+  @Override
+  protected void initCoCoChecker() {
+    checker.addCoCo(new ReferencedTypeExistsCoco());
+  }
+
+  @Override
+  protected List<String> getErrorCodeOfCocoUnderTest() {
+    return Arrays.asList("0xB0028");
+  }
+
+  @Test
+  public void testCocoViolation() {
+//    testCocoViolation("", 0, 0);
+  }
+}
