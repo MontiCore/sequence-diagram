a4ac9100e6068bf0069636455b05dc9cd7150e52
Kautz <kautz@se-rwth.de>
2020-09-10 17:58:08 +0200

    overwritten getName of SD4DevelopmentArtifactScope for resolving since DiagramSymbol is no top-level symbol anymore

---
+++ src/main/java/de/monticore/lang/sd4development/_symboltable/SD4DevelopmentArtifactScope.java
@@ -0,0 +1,24 @@
+package de.monticore.lang.sd4development._symboltable;
+
+import de.monticore.lang.sd4development._symboltable.SD4DevelopmentArtifactScopeTOP;
+import de.monticore.symboltable.ImportStatement;
+
+import java.util.List;
+import java.util.Optional;
+
+public class SD4DevelopmentArtifactScope extends SD4DevelopmentArtifactScopeTOP {
+
+  public SD4DevelopmentArtifactScope(String packageName, List<ImportStatement> imports) {
+    super(packageName, imports);
+  }
+
+  public SD4DevelopmentArtifactScope(Optional<ISD4DevelopmentScope> enclosingScope, String packageName, List<ImportStatement> imports) {
+    super(enclosingScope, packageName, imports);
+  }
+
+  @Override
+  public String getName() {
+    // every SD contains exactly one diagram symbol
+    return getLocalDiagramSymbols().get(0).getName();
+  }
+}
