d5e811e37f7376272a73b624d4085f844905df18
Oliver Kautz <oliver.kautz@rwth-aachen.de>
2021-05-11 07:22:18 +0000

    minor
    minor

--- pre/mirror.sh
+++ pre/mirror.sh
@@ -4,9 +4,4 @@ do
   sed -i 's/\[\[_TOC_\]\]//' $file
 done
 echo "[INFO] Removed all occureneces of '[[_TOC_]]' in *.md files"
-# Replace descriptions of not publicly reachable RWTH GitLab links
-for file in $(find . -type f -name "*.md")
-do
-  content=$(java -jar MDLinkChangerCLI.jar -f $file)
-  echo $content > $file
-done
+
