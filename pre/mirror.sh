40e3a5325bd06331ff5a2c04d002470f080987a1
Oliver Kautz <oliver.kautz@rwth-aachen.de>
2021-05-11 07:22:18 +0000

    Update mirror.sh

--- pre/mirror.sh
+++ pre/mirror.sh
@@ -4,4 +4,10 @@ do
   sed -i 's/\[\[_TOC_\]\]//' $file
 done
 echo "[INFO] Removed all occureneces of '[[_TOC_]]' in *.md files"
-
+# Replace descriptions of not publicly reachable RWTH GitLab links
+curl --location --header "PRIVATE-TOKEN: jU7hPx74kzmMGFjfGmHC" "https://git.rwth-aachen.de/api/v4/projects/monticore%2Fmdlinkchecker/jobs/artifacts/master/raw/target/libs/MDLinkChangerCLI.jar?job=build" --output MDLinkChangerCLI.jar
+for file in $(find . -type f -name "*.md")
+do
+  content=$(java -jar MDLinkChangerCLI.jar -f $file)
+  echo $content > $file
+done
