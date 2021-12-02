7a0cdc45de9106de3ef075ef8fe832bead4bdd23
Leon Linhart <leon.linhart@rwth-aachen.de>
2021-12-02 12:42:22 +0100

    Provide basic SD2Java infrastructure
    The SD2CDTransformer will be implemented in future updates.

---
+++ src/main/java/de/monticore/lang/sd4development/sd2cd/SD2CDTransformer.java
@@ -0,0 +1,13 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.lang.sd4development.sd2cd;
+
+import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
+import de.monticore.lang.sdbasis._ast.ASTSDArtifact;
+
+public class SD2CDTransformer {
+
+  public ASTCDCompilationUnit transform(ASTSDArtifact ast) {
+    throw new Error("Not yet implemented");
+  }
+
+}
